96abb8e93fdf79aed3238b25b9692fe201b1d5aa
gargpriyal10 <priyal.garg_cs.aiml24@gla.ac.in>
2026-02-11 09:31:40 +0530

    add screen width and screen height.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
 #include <Arduino.h>
+#include <wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET 0x3c
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
 void setup() {
 // write your initialization code here
 
